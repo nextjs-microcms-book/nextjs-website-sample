2956d5338e8e421e29d6d00345709f1526ce8bac
YouheiNozaki <youhey.hey0505@gmail.com>
2024-04-07 03:42:49 +0900

    3章：Footerのリンクもnext/linkに修正

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './index.module.css';
 
 export default function Footer() {
@@ -6,13 +7,13 @@ export default function Footer() {
       <nav className={styles.nav}>
         <ul className={styles.items}>
           <li className={styles.item}>
-            <a href="/news">ニュース</a>
+            <Link href="/news">ニュース</Link>
           </li>
           <li className={styles.item}>
-            <a href="/members">メンバー</a>
+            <Link href="/members">メンバー</Link>
           </li>
           <li className={styles.item}>
-            <a href="/contact">お問い合わせ</a>
+            <Link href="/contact">お問い合わせ</Link>
           </li>
         </ul>
       </nav>
