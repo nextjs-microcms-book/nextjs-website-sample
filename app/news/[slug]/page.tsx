ae710de0c4f9cdcdc7a2a2f1690446e562fbb21f
Kazuki Shibata <shibata@microcms.co.jp>
2024-04-14 22:14:08 +0900

    8章：全体にrevalidate=60を指定

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -13,8 +13,6 @@ type Props = {
   };
 };
 
-export const revalidate = 60;
-
 export default async function Page({ params, searchParams }: Props) {
   const data = await getNewsDetail(params.slug, {
     draftKey: searchParams.dk,
--- app/news/layout.tsx
+++ app/news/layout.tsx
@@ -5,6 +5,8 @@ type Props = {
   children: React.ReactNode;
 };
 
+export const revalidate = 60;
+
 export default function NewsLayout({ children }: Props) {
   return (
     <>
