a8d883d3fe971959c6e84889f0e08aa9cf2c14bc
Kazuki Shibata <shibata@microcms.co.jp>
2024-04-14 09:21:02 +0900

    8章：ニュース一覧と詳細にrevalidateを指定

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -13,6 +13,8 @@ type Props = {
   };
 };
 
+export const revalidate = 0;
+
 export default async function Page({ params, searchParams }: Props) {
   const data = await getNewsDetail(params.slug, {
     draftKey: searchParams.dk,
