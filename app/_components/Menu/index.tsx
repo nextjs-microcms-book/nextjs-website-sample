3078d0f95642e8cfa64872984675dea49a9943db
YouheiNozaki <youhey.hey0505@gmail.com>
2024-04-14 18:00:23 +0900

    4-4-3: ハンバーガーメニューのアクセシビリティ対応

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -3,40 +3,39 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
-import cx from 'classnames';
 import styles from './index.module.css';
 
 export default function Menu() {
   const [isOpen, setOpen] = useState<boolean>(false);
-  const open = () => setOpen(true);
-  const close = () => setOpen(false);
+  const toggle = () => setOpen(!isOpen);
 
   return (
     <div>
-      <nav className={cx(styles.nav, isOpen && styles.open)}>
+      <nav className={styles.nav} id='navigation' aria-hidden={!isOpen}>
         <ul className={styles.items}>
           <li>
-            <Link href="/news">ニュース</Link>
+            <Link href='/news'>ニュース</Link>
           </li>
           <li>
-            <Link href="/members">メンバー</Link>
+            <Link href='/members'>メンバー</Link>
           </li>
           <li>
-            <Link href="/contact">お問い合わせ</Link>
+            <Link href='/contact'>お問い合わせ</Link>
           </li>
         </ul>
-        <button className={cx(styles.button, styles.close)} onClick={close}>
-          <Image
-            src="/close.svg"
-            alt="閉じる"
-            width={24}
-            height={24}
-            priority
-          />
-        </button>
       </nav>
-      <button className={styles.button} onClick={open}>
-        <Image src="/menu.svg" alt="メニュー" width={24} height={24} />
+      <button
+        type='button'
+        aria-controls='navigation'
+        aria-expanded={isOpen}
+        className={styles.button}
+        onClick={toggle}
+      >
+        {isOpen ? (
+          <Image src='/close.svg' alt='閉じる' width={24} height={24} />
+        ) : (
+          <Image src='/menu.svg' alt='メニュー' width={24} height={24} />
+        )}
       </button>
     </div>
   );
