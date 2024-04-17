f5530fe72afe545ecd4093fab43ca410d049c8e4
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2024-04-17 18:44:37 +0900

    9章まで完了

--- app/_actions/contact.ts
+++ app/_actions/contact.ts
@@ -52,5 +52,54 @@ export async function createContactData(_prevState: any, formData: FormData) {
     };
   }
 
+  const result = await fetch(
+    `https://api.hsforms.com/submissions/v3/integration/submit/${process.env.HUBSPOT_PORTAL_ID}/${process.env.HUBSPOT_FORM_ID}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fields: [
+          {
+            objectTypeId: "0-1",
+            name: "lastname",
+            value: rawFormData.lastname,
+          },
+          {
+            objectTypeId: "0-1",
+            name: "firstname",
+            value: rawFormData.firstname,
+          },
+          {
+            objectTypeId: "0-1",
+            name: "company",
+            value: rawFormData.company,
+          },
+          {
+            objectTypeId: "0-1",
+            name: "email",
+            value: rawFormData.email,
+          },
+          {
+            objectTypeId: "0-1",
+            name: "message",
+            value: rawFormData.message,
+          },
+        ],
+      }),
+    }
+  );
+
+  try {
+    await result.json();
+  } catch (e) {
+    console.log(e);
+    return {
+      status: "error",
+      message: "お問い合わせに失敗しました",
+    };
+  }
+
   return { status: "success", message: "OK" };
 }
