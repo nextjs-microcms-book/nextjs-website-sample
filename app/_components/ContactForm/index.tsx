6486a4db087f8f8a5474c0ff62463f7e03f2b1c0
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2024-04-17 18:40:20 +0900

    お問い合わせページにServerActionsを紐づけてロジックを追加

--- app/_components/ContactForm/index.tsx
+++ app/_components/ContactForm/index.tsx
@@ -1,8 +1,28 @@
+"use client";
+
+import { createContactData } from "@/app/_actions/contact";
+import { useFormState } from "react-dom";
 import styles from "./index.module.css";
 
+const initialState = {
+  status: "",
+  message: "",
+};
+
 export default function ContactForm() {
+  const [state, formAction] = useFormState(createContactData, initialState);
+  console.log(state);
+  if (state.status === "success") {
+    return (
+      <p className={styles.success}>
+        お問い合わせいただき、ありがとうございます。
+        <br />
+        お返事まで今しばらくお待ちください。
+      </p>
+    );
+  }
   return (
-    <form className={styles.form}>
+    <form className={styles.form} action={formAction}>
       <div className={styles.horizontal}>
         <div className={styles.item}>
           <label className={styles.label} htmlFor="lastname">
@@ -56,6 +76,9 @@ export default function ContactForm() {
         <textarea className={styles.textarea} id="message" name="message" />
       </div>
       <div className={styles.actions}>
+        {state.status === "error" && (
+          <p className={styles.error}>{state.message}</p>
+        )}
         <input type="submit" value="送信する" className={styles.button} />
       </div>
     </form>
